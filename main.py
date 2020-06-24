8290a2bb3e60ae2323daca513fa530e5b0a77a20
Sasha Tripathi <40964328+TDLPoof@users.noreply.github.com>
2020-06-23 23:39:45 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import config
 from game_state import GameState
 from game import Game
 
+pygame.mixer.music.load("audio/bgMusic.wav")
+pygame.mixer.music.play(-1)
+
 def main(width = 640, height = 640, frameRate = 50):
     pygame.init()
     screen = pygame.display.set_mode((width, height))
